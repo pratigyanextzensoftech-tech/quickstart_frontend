38ad10243b86a7449001e1d0a8ece84faa1ed9b9
unknown <pratigya.nextzensoftech@gmail.com>
2026-01-16 12:34:01 +0530

    commit msg1

--- src/Components/Endpoint/index.tsx
+++ src/Components/Endpoint/index.tsx
@@ -25,8 +25,12 @@ const Endpoint = (props: Props) => {
   const [isLoading, setIsLoading] = useState(false);
 
   const getData = async () => {
+      const API_BASE =
+  process.env.NODE_ENV === "production"
+    ? "https://quickstart-1.onrender.com"
+    : "http://127.0.0.1:8000";
     setIsLoading(true);
-    const response = await fetch(`/api/${props.endpoint}`, { method: "GET" });
+    const response = await fetch(`${API_BASE}/api/${props.endpoint}`, { method: "GET" });
     const data = await response.json();
     if (data.error != null) {
       setError(data.error);
